a4ffab5f3381453d8f4d4422ff140894cf066216
LuisNava147 <luis.navas147@gmail.com>
2025-11-22 19:14:55 -0600

    Feature: conexion BD parte 2(creacion en postgres)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+    type: "postgres",
+      host: process.env.host,
+      port: 5438,
+      username: "postgres",
+      password: "BD2023",
+      database: process.env.name,
+      entities:[],
+      autoLoadEntities: true,
+      synchronize: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
