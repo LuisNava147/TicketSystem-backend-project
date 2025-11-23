931b158dffa67349d79c1b07aceb11eb82c2c326
LuisNava147 <luis.navas147@gmail.com>
2025-11-23 00:44:22 -0600

    Feature: Create-Location-Dto, location.controller & location.service (POST, GET, GET BY, PATH & DELETE)

--- src/locations/dto/create-location.dto.ts
+++ src/locations/dto/create-location.dto.ts
@@ -1,1 +1,8 @@
-export class CreateLocationDto {}
+import { IsString,MaxLength,MinLength, IsEmail,IsOptional, IsArray, ArrayNotEmpty,IsObject, IsUUID} from "class-validator";
+
+export class CreateLocationDto {
+@IsString()
+@MinLength(4)
+locationName: string
+
+}
--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -1,26 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Location } from './entities/location.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class LocationsService {
-  create(createLocationDto: CreateLocationDto) {
-    return 'This action adds a new location';
+  constructor(
+    @InjectRepository(Location)
+    private readonly locationRepository: Repository<Location>,
+  ){}
+
+  async create(createLocationDto: CreateLocationDto) {
+    try{
+      const location = this.locationRepository.create(createLocationDto)
+      await this.locationRepository.save(location)
+      return location;
+    }catch(error){
+      if(error.code == '23505'){
+        throw new BadRequestException(`El estado ${createLocationDto.locationName} ya existe`);
+      }
+      throw new InternalServerErrorException('Error al crear location');
+    }
   }
 
   findAll() {
-    return `This action returns all locations`;
+    return this.locationRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} location`;
+  async findOne(id: number) {
+      const location = await this.locationRepository.findOneBy({
+        locationId: id,
+      })
+      if(!location)throw new NotFoundException('Locación no encontrada');
+      return location;
   }
 
-  update(id: number, updateLocationDto: UpdateLocationDto) {
-    return `This action updates a #${id} location`;
+  async update(id: number, updateLocationDto: UpdateLocationDto) {
+    const location = await this.locationRepository.preload({
+      locationId: id,
+      ...updateLocationDto,
+    })
+    if(!location)throw new NotFoundException ('La locación no se puede actualizar')
+    try{
+      await this.locationRepository.save(location)
+      return location;
+  }catch(error){
+    if(error.code == '23505'){
+      throw new BadRequestException(`Ya existe la locación '${updateLocationDto.locationName}'`)
+    }
+    throw new InternalServerErrorException('Error al actualizar la locación')
+  }
   }
 
   remove(id: number) {
-    return `This action removes a #${id} location`;
+    return this.locationRepository.delete({
+      locationId: id
+    });
   }
 }
