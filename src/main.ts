e91aae10fad25a228a91562dee3dc37ccb48aa95
LuisNava147 <luis.navas147@gmail.com>
2025-11-30 00:38:44 -0600

    Feature: preparación en main.ts para hacer frontend

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: process.env.allowedOrigin,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  })
+
+  app.use(bodyParser.json({limit: '10mb'}));
+  app.use(bodyParser.urlencoded({limit:'10mb', extended:true}));
+
+  const config = new DocumentBuilder()
+  .setTitle('Chihuahuenos API')
+  .setDescription('API de venta de boletos')
+  .setVersion('0.9')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs',app, document);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  }));
+
   await app.listen(3010);
 
   app.useGlobalPipes(new ValidationPipe({
