7ab35eec2e8184ac271fa59f263969778270e948
LuisNava147 <luis.navas147@gmail.com>
2025-11-30 00:12:59 -0600

    Feature: AWS cargar y actualizar imgs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,8 @@ import { UseGuards } from '@nestjs/common/decorators';
 import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from 'src/auth/decorators/roles.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
+import { UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('users')
 export class UsersController {
@@ -16,6 +18,26 @@ export class UsersController {
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
+
+  @Post('upload/:id')
+  @UseGuards(AuthGuard('jwt'))
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @Param('id',ParseUUIDPipe)id: string,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({maxSize:1024*1024*2}),
+          new FileTypeValidator({fileType: '.(png|jpeg|jpg)'}),
+        ],
+      })
+    )
+    file: Express.Multer.File,
+  ){
+    return this.usersService.uploadIdentityDocument(id, file);
+  }
+ 
+
 
   @Get()
   @UseGuards(AuthGuard('jwt'),RolesGuard)
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,9 +5,19 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
+import { PutObjectCommand, S3Client, DeleteObjectCommand } from '@aws-sdk/client-s3';
+import { v4 as uuidv4 } from 'uuid';
+
 
 @Injectable()
 export class UsersService {
+  private readonly s3Client = new S3Client({
+    region: process.env.bucket_region!,
+    credentials:{
+      accessKeyId: process.env.accesskey_bucket!,
+      secretAccessKey: process.env.secretkey_bucket!,
+    }
+  });
   constructor(
     @InjectRepository(User)
     private readonly userReposity: Repository<User>,
@@ -76,5 +86,39 @@ export class UsersService {
       where:{userEmail: email},
       select: ['userId','userEmail','userPassword','userFullName','roles']
     })
+  }
+
+  async uploadIdentityDocument(id:string, file: Express.Multer.File){
+    const user = await this.findOne(id);
+    if(user.indetifyDocumentUrl){
+      const oldFileName = user.indetifyDocumentUrl.split('/').pop();
+      try{
+        await this.s3Client.send(
+          new DeleteObjectCommand({
+            Bucket: process.env.bucket_name,
+            Key: oldFileName
+          }),
+        );
+        //console.log(`Archivo anterior eliminado: ${oldFileName}`);
+      }catch(error){
+        console.warn('No se puede borrar el archivo anterior, ',error);
+      }
+    }
+    
+    const fileExtension = file.originalname.split('.').pop();
+    const fileName= `${uuidv4()}.${fileExtension}`;
+
+    await this.s3Client.send(
+      new PutObjectCommand({
+        Bucket: process.env.bucket_name,
+        Key: fileName,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+        ACL: 'public-read',
+      })
+    );
+    const url = `https://${process.env.bucket_name}.s3.amazonaws.com/${fileName}`;
+    user.indetifyDocumentUrl = url
+    return await this.userReposity.save(user);
   }
 }
